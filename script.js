6ae5eb6846ddcf269b2bd66bc9416a831ffc2a1e
majuda <majudan12@gmail.com>
2020-07-25 17:49:32 -0400

    added code

---
+++ script.js
@@ -0,0 +1,19 @@
+function myfunction() {
+
+  //TASK 2
+  
+  //This is a comment, you can write it in 2 ways the first way is demonstrated here
+  //but I have to type "//" 2 foward slashes on each line
+  //what is the second way to add a comment in javascript? demonstrate it below
+
+
+    //THIS IS YOUR HINT FOR TASK 1, it should not work
+  console.log(document.getElement);
+
+
+  //This is how you view logs in the console, you open index.html in chrome then inspect then press the console tab
+  console.log("hello world");
+
+}
+
+myfunction();
